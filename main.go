bfca3571c712c1ffd0b105f03eaeb64e7abea0ef
Akella313 <abuakella@yandex.ru>
2026-04-06 17:26:31 +0300

    Step 1. Added server

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "final-project/pkg/server"
+
+func main() {
+	// Запускает сервер
+	server.Run()
+}
---
+++ pkg/server/server.go
@@ -0,0 +1,18 @@
+package server
+
+import (
+	"net/http"
+	"os"
+)
+
+// Функция Run() запускает сервер с портом из переменной окружения TODO_PORT
+// Если переменная не задана, используется порт по умолчанию 7540
+func Run() {
+	port := os.Getenv("TODO_PORT")
+	if port == "" {
+		port = "7540"
+	}
+
+	http.Handle("/", http.FileServer(http.Dir("./web")))
+	http.ListenAndServe(":"+port, nil)
+}
